27a4d4571877d1f2ecc6e0f408ea0e7723f2de82
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2024-10-23 16:59:47 +0200

    alt tag

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function HomePage() {
   return (
     <div className="home-pic-container">
-        <img src="/Pictures/HomePagePic/djecanasnijegu.jpg" alt="Picture of ski track." />
+        <img src="/Pictures/HomePagePic/djecanasnijegu.jpg" alt="Pic of ski track." />
     </div>
   )
 }

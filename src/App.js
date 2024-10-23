d5efc4e8e10cab2234b6b50c7dc16960fb42bdb9
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2024-10-23 14:34:35 +0200

    added homePage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import PageLayout from "./PageLayout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PageLayout />}>
+          <Route index element={<HomePage />} />
         </Route>
       </Routes>
     </BrowserRouter>

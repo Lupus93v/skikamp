edccbb2fb8618bb6b32dc7dc13bc8b3fccf5a006
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2024-10-25 14:04:43 +0200

    Responsivecss mostly
    main page and ubout us page

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -3,16 +3,21 @@ import { NavLink, Outlet } from "react-router-dom"
 
 export default function AboutUs() {
 
-const activeStyles = {
-    fontWeight: "bold",
-    fontSize: "1.5rem"
-}
+    const activeStyles = {
+        fontWeight: "bold",
+        fontSize: "1.3rem",
+        textDecoration: "underline",
+    }
 
     return (
-        <div className="about-us">
-            <NavLink className="about-us-btns" style={({isActive}) => isActive ? activeStyles : null} to="." end>Тренери</NavLink>
-            <NavLink className="about-us-btns" style={({isActive}) => isActive ? activeStyles : null} to="AboutClub">О Удружењу и школи</NavLink>
-            <Outlet />
+        <div className="about-us-container">
+            <div className="about-us-btns">
+                <NavLink className="about-us-btn" style={({ isActive }) => isActive ? activeStyles : null} to="." end>Тренери</NavLink>
+                <NavLink className="about-us-btn" style={({ isActive }) => isActive ? activeStyles : null} to="AboutClub">О клубу и школи</NavLink>
+            </div>
+            <div>
+                <Outlet />
+            </div>
         </div>
     )
 }

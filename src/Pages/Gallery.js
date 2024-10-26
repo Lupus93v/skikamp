cedd0bf1a45be1344326a1a3f550fc7ed33c07ab
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2024-10-26 16:17:42 +0200

    adjustments

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -3,6 +3,8 @@ import React from "react"
 export default function Gallery() {
 
     return (
-        <h1>ГАЛЕРИЈА (израда у току)</h1>
+        <div className="gallery-container">
+            <h1>ГАЛЕРИЈА (израда у току)</h1>
+        </div>
     )
 }
--- src/Pages/Sponsors.js
+++ src/Pages/Sponsors.js
@@ -3,6 +3,8 @@ import React from "react"
 export default function Sponsors() {
 
     return (
+        <div className="sponsors">
         <h1>ПРИЈАТЕЉИ КЛУБА (израда у току)</h1>
+        </div>
     )
 }

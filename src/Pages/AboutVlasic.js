6c613ae351b8d9e921ba33b068e23d5bf8cff35e
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2024-10-26 13:47:15 +0200

    about vlsaic page
    eddited page About Vlasic.

--- src/Pages/AboutVlasic.js
+++ src/Pages/AboutVlasic.js
@@ -3,6 +3,32 @@ import React from "react"
 export default function AboutVlasic() {
 
     return (
-        <h1>О ВЛАШИЋУ (израда у току)</h1>
+        <div className="about-vlasic">
+            <h4>О Влашићу</h4>
+            <div className="about-vlasic-pic-txt">
+                <div className="first-column">
+                    <img src="./Pictures/AboutVlasicPic/12.png" />
+                    <img src="./Pictures/AboutVlasicPic/9.jpg" />
+                    <img src="./Pictures/AboutVlasicPic/10.jpg" />
+                    <img src="./Pictures/AboutVlasicPic/11.png" />
+                    <img src="./Pictures/AboutVlasicPic/14.png" />
+                    <img src="./Pictures/AboutVlasicPic/8.png" />
+                </div>
+                <div className="about-vlasic-txt">
+                    <p>Влашић је планинси масив који доминира Средњом Босном, чији је највиши врх Паљеник на 1.943 метра надморске висине. Најпитомији и за туристе најинтересантнији дио Влашића је познат као Бабановац. Смјештен је на висини од 1.260 метара.</p>
+                    <br /><p>Бабановац је уствари окосница туристичког развоја, па је још седамдесетих година на овом дијелу изграђена 90 метара висока скакаоница на којој су се одржавала међународна такмичења у скосковима "Покал Влашић".</p>
+                    <br /><p>Скијашки апетити на Влашићу углавном могу бити задовољени са шест ски лифтова и стазама у укупној дужини око 14 километара. Углавном су стазе на Влашићу погодне за рекреативце, док стаза Марковац задовољава услове напреднијег скијања. Почетницима су на располагању два бејби ски лифта и 500 метара уређене стазе.
+                    </p>
+                </div>
+                <div className="second-column">
+                    <img src="./Pictures/AboutVlasicPic/6.png" />
+                    <img src="./Pictures/AboutVlasicPic/3.jpg" />
+                    <img src="./Pictures/AboutVlasicPic/7.jpg" />
+                    <img src="./Pictures/AboutVlasicPic/2.jpg" />
+                    <img src="./Pictures/AboutVlasicPic/1.png" />
+                    <img src="./Pictures/AboutVlasicPic/13.png" />
+                </div>
+            </div>
+        </div>
     )
 }

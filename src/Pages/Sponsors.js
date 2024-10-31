96f7d9e18fa301f57157c5750eff762d0e031135
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2024-10-31 12:28:32 +0100

    alt fix

--- src/Pages/Sponsors.js
+++ src/Pages/Sponsors.js
@@ -5,22 +5,22 @@ export default function Sponsors() {
     return (
         <div className="sponsors">
             <div className="sponsor">
-                <img src="./Pictures/Sponsors/prnjavor.png" />
+                <img src="./Pictures/Sponsors/prnjavor.png" alt="sponsor img" />
             </div>
             <div className="sponsor">
-                <img src="./Pictures/Sponsors/krunaprodukt.jpeg" />
+                <img src="./Pictures/Sponsors/krunaprodukt.jpeg" alt="sponsor img" />
             </div>
             <div className="sponsor">
-                <img src="./Pictures/Sponsors/trivas.jpg" />
+                <img src="./Pictures/Sponsors/trivas.jpg" alt="sponsor img" />
             </div>
             <div className="sponsor">
-                <img src="./Pictures/Sponsors/tehnoplast.png" />
+                <img src="./Pictures/Sponsors/tehnoplast.png" alt="sponsor img" />
             </div>
             <div className="sponsor">
-                <img src="./Pictures/Sponsors/tehnoprint.jpeg" />
+                <img src="./Pictures/Sponsors/tehnoprint.jpeg" alt="sponsor img" />
             </div>
             <div className="sponsor">
-                <img src="./Pictures/Sponsors/ekofis.jpeg" />
+                <img src="./Pictures/Sponsors/ekofis.jpeg" alt="sponsor img" />
             </div>
         </div>
     )

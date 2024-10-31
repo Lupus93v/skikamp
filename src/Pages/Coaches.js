e0b5e1e310ef1ac4bb2133fc493dc514f2f116a0
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2024-10-31 10:21:23 +0100

    fix and add
    Fixing homepage pic container. Addig images of coaches.

--- src/Pages/Coaches.js
+++ src/Pages/Coaches.js
@@ -5,17 +5,17 @@ export default function Coaches() {
     return (
         <div className="coaches-container">
             <div className="poster">
-                <img src="./Pictures/test.png" alt="Pic of choac." />
+                <img src="./Pictures/Coaches/deks.jpg" alt="Pic of choac." />
                 <h5>Дејан Благојевић</h5>
                 <h6>Нови Град</h6>
             </div>
             <div className="poster">
-                <img src="./Pictures/test.png" alt="Pic of choac." />
+                <img src="./Pictures/Coaches/malba.png" alt="Pic of choac." />
                 <h5>Бојан Малбашић</h5>
                 <h6>Прњавор</h6>
             </div>
             <div className="poster">
-                <img src="./Pictures/test.png" alt="Pic of choac." />
+                <img src="./Pictures/Coaches/aco.JPEG" alt="Pic of choac." />
                 <h5>Александар Сантрач</h5>
                 <h6>Приједор</h6>
             </div>
@@ -25,22 +25,22 @@ export default function Coaches() {
                 <h6>Нови Град</h6>
             </div>
             <div className="poster">
-                <img src="./Pictures/test.png" alt="Pic of choac." />
+                <img src="./Pictures/Coaches/duri.jpg" alt="Pic of choac." />
                 <h5>Дарко Дуроњић</h5>
                 <h6>Прњавор</h6>
             </div>
             <div className="poster">
-                    <img src="./Pictures/test.png" alt="Pic of choac." />
+                    <img src="./Pictures/Coaches/marijana.jpg" alt="Pic of choac." />
                     <h5>Маријана Граховац</h5>
-                    <h5>Козарска Дубица</h5>
+                    <h6>Козарска Дубица</h6>
                 </div>
                 <div className="poster">
-                    <img src="./Pictures/test.png" alt="Pic of choac." />
+                    <img src="./Pictures/Coaches/nemanja.jpg" alt="Pic of choac." />
                     <h5>Немања Дмитрашиновић</h5>
-                    <h5>Козарска Дубица</h5>
+                    <h6>Козарска Дубица</h6>
                 </div>
             <div className="poster">
-                <img src="./Pictures/test.png" alt="Pic of choac." />
+                <img src="./Pictures/Coaches/vuk.jpg" alt="Pic of choac." />
                 <h5>Вук Благојевић</h5>
                 <h6>Нови Град</h6>
             </div>

2e7db3707b540382677cad9ae8d9fac865143ec6
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2024-11-05 13:30:26 +0100

    fix

--- src/Pages/Coaches.js
+++ src/Pages/Coaches.js
@@ -6,7 +6,7 @@ export default function Coaches() {
         <div className="coaches-page">
             <h3 style={{marginBottom: "1em", textAlign: "center"}}>Гостовање нашег тренера, Бојана Малбашића, на ТВ К3.</h3>
         <div className="video">
-        <iframe width="100%" height="auto" src="https://www.youtube.com/embed/DYfoNMPSAZo?si=tvBaxmQK4CVQ4xsy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe style={{padding: "0.5em", border: "solid black 1px", borderRadius: "6px"}}width="100%" height="auto" src="https://www.youtube.com/embed/DYfoNMPSAZo?si=tvBaxmQK4CVQ4xsy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
 
         <div className="coaches-container">

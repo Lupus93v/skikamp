08220a96f39757c75dac387bea16442691b53803
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2024-12-11 09:35:42 +0100

    apply form

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -50,7 +50,7 @@ export default function DropdownMenu() {
                 <NavLink className="dropdownItem" to="/aboutus" onClick={closeDropdown}>О нама</NavLink>
                 <NavLink className="dropdownItem" to="/aboutvlasic" onClick={closeDropdown}>О Влашићу</NavLink>
                 <NavLink className="dropdownItem" to="/apply" onClick={closeDropdown}>Пријави се</NavLink>
-                
+                <NavLink className="dropdownItem" to="/applyform" onClick={closeDropdown}>Пријави се</NavLink>
             </div>
         )
     }

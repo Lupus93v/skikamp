1a0e63c087bfa3542c6955c5475a44373c85bbeb
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2024-12-11 17:51:16 +0100

    phone numbers

--- src/Pages/Coaches.js
+++ src/Pages/Coaches.js
@@ -14,41 +14,49 @@ export default function Coaches() {
                 <img src="./Pictures/Coaches/deks.jpg" alt="Pic of choac." />
                 <h5>Дејан Благојевић</h5>
                 <h6>Нови Град</h6>
+                <p>065/473-399</p>
             </div>
             <div className="poster">
                 <img src="./Pictures/Coaches/malba.png" alt="Pic of choac." />
                 <h5>Бојан Малбашић</h5>
                 <h6>Прњавор</h6>
+                <p>065/863-346</p>
             </div>
             <div className="poster">
                 <img src="./Pictures/Coaches/aco.JPEG" alt="Pic of choac." />
                 <h5>Александар Сантрач</h5>
                 <h6>Приједор</h6>
+                <p>065/782-650</p>
             </div>
             <div className="poster">
                 <img src="./Pictures/test.png" alt="Pic of choac." />
                 <h5>Љубиша Дрљача</h5>
                 <h6>Нови Град</h6>
+                <p>065/993-710</p>
             </div>
             <div className="poster">
                 <img src="./Pictures/Coaches/duri.jpg" alt="Pic of choac." />
                 <h5>Дарко Дуроњић</h5>
                 <h6>Прњавор</h6>
+                <p>065/859-645</p>
             </div>
             <div className="poster">
                     <img src="./Pictures/Coaches/marijana.jpg" alt="Pic of choac." />
                     <h5>Маријана Граховац</h5>
                     <h6>Козарска Дубица</h6>
+                    <p>065/413-212</p>
                 </div>
                 <div className="poster">
                     <img src="./Pictures/Coaches/nemanja.jpg" alt="Pic of choac." />
                     <h5>Немања Дмитрашиновић</h5>
                     <h6>Козарска Дубица</h6>
+                    <p>066/935-287</p>
                 </div>
                 <div className="poster">
                     <img src="./Pictures/Coaches/srdjan.png" alt="Pic of choac." />
                     <h5>Срђан Дејановић</h5>
                     <h6>Бања Лука</h6>
+                    <p>065/543-707</p>
                 </div>
             <div className="poster">
                 <img src="./Pictures/Coaches/vuk.jpg" alt="Pic of choac." />

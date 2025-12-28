662025bfd573e995a8d1f49f54ac87f26ad156ad
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2025-12-28 15:59:54 +0100

    fix
    fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from "react"
 export default function Footer() {
     return (
         <div className="footer">
-            <a href="https://lupussolutions.netlify.app" target="_blank">Powered by: Lupus Solutions</a>
-            <a id="policy" href="/privacy-policy" target="_blank">Privacy Policy</a>
+            <a href="https://lupussolutions.netlify.app" target="_blank" rel="noopener noreferrer">Powered by: Lupus Solutions</a>
+            <a id="policy" href="/privacy-policy" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
         </div>
     )
 }

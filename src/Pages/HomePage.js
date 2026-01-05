0b21fb4c2025506d740d75921e3838ea4c2ed9b5
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2026-01-05 21:03:56 +0100

    termini
    termini

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,6 +5,7 @@ import { NavLink } from "react-router-dom"
 
 export default function HomePage() {
   const [isHovered, setIsHovered] = useState(false);
+  const [showBubble, setShowBubble] = useState(false);
 
   return (
     <div className="home-page-container">
@@ -20,13 +21,8 @@ export default function HomePage() {
             className="static-image"
           />
       </div>
-      <h3 class="animate_animated">Инспиришемо нове генерације скијаша од 2016. године!</h3>
-      <h2 style={{
-        color: "darkorange",
-        fontSize: "1.3rem",
-        marginTop: "10px",
-        marginBottom: "20px"
-      }}>СЕЗОНА 2026 ЈЕ ОТВОРЕНА!</h2>
+      <h3 className="animate__animated">Инспиришемо нове генерације скијаша од 2016. године!</h3>
+      <h2>СЕЗОНА 2026 ЈЕ ОТВОРЕНА!</h2>
       <div style={{
         display: "flex",
       }}>
@@ -40,7 +36,7 @@ export default function HomePage() {
           }}>
         <img style={{width: "40%", float: "right"}}src="./Pictures/HomePagePic/gostovanje.jpg" alt="guest at tv show"/>
       <a
-        class="animate__animated animate__zoomInDown"
+        className="animate__animated animate__zoomInDown"
         href="https://youtu.be/DYfoNMPSAZo?si=XdkXmuGsKQ5DN3wr"
         target="_blank"
         rel="noopener noreferrer"
@@ -64,11 +60,24 @@ export default function HomePage() {
         height: "110px",
         marginTop: "-1px"
       }}>
-        <button id="home-apply-btn">ВИДИ ТЕРМИНЕ</button>
+        <button className="info-btn" onClick={() => setShowBubble(!showBubble)} id="home-apply-btn">ВИДИ ТЕРМИНЕ</button>
+        {showBubble && (
+        <div className="bubble">
+          <p>
+            3. јануар - 5. јануар<br />
+            8. јануар - 10. јануар<br /> 
+            11. јануар - 13. јануар<br />
+            15. јануар - 17. јануар<br />
+            23. јануар - 25. јануар<br />
+            30. јануар - 1. фебруар<br />
+            6. фебруар - 8. фебруар
+          </p></div>
+        )}
         <NavLink id="home-apply-btn" to="/applyform">ПРИЈАВИ СЕ</NavLink>
+      
       </div>
       </div>
       <SponsorSlider />
     </div>
-  )
+  );
 }

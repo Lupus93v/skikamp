d478985fd79d84fb943b9781e74d74d266b4e1fa
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2026-01-05 19:15:28 +0100

    fix
    fix

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {useState, useEffect} from "react"
+import {useState} from "react"
 import SponsorSlider from "../components/SponsorSlider";
 import { NavLink } from "react-router-dom"
 
